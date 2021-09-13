509944096570c2e27d2ba138ea141860346101b7
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2021-09-13 20:43:20 +0000

    file change

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
+from werkzeug.serving import run_simple # werkzeug development server
 from tool_repository.requests import app
 
-application = DispatcherMiddleware(app)
+
+
+
+def set_up_application():
+    application = DispatcherMiddleware(app)
+    return application
+
+
+if __name__ == '__main__':
+    run_simple('localhost', 5000, set_up_application())
--- run.py
+++
@@ -1,4 +0,0 @@
-from werkzeug.serving import run_simple # werkzeug development server
-from app import application
-if __name__ == '__main__':
-    run_simple('localhost', 5000, application)
