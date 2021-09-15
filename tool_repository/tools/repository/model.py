701df8bb4c52d16c960383c3d92accffc13a54c1
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2021-09-15 04:05:31 +0000

    definiton class functions

--- tool_repository/tools/repository/model.py
+++ tool_repository/tools/repository/model.py
@@ -169,6 +169,20 @@ class Definition(Base):
     FileName = Column("FileName", String(128), nullable=False)
     Definition = Column("Definition", Text, nullable=True)
 
+    def __init__(self, assignment_information: dict) -> None:
+        self.DefinitionId = assignment_information.get("DefinitionId")
+        self.ClassName = assignment_information.get("ClassName")
+        self.FileName = assignment_information.get("FileName")
+        self.Definition = assignment_information.get("Definition")
+    
+    def to_dict(self) -> dict:
+        return {
+            "DefinitionId": self.DefinitionId,
+            "ClassName": self.ClassName,
+            "FileName": self.FileName,
+            "Definition": self.Definition,
+        }
+
 
 def init_db():
     global Base, Engine
