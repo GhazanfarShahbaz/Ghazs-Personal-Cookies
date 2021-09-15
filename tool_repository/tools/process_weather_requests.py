6214bac26306e7a9325f073d775b0d9db8e4d4dc
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2021-09-15 05:16:27 +0000

    weather api

--- tool_repository/tools/process_weather_requests.py
+++ tool_repository/tools/process_weather_requests.py
@@ -8,4 +8,4 @@ def get_weather() -> dict:
     weather_zip_code: str = os.environ["WEATHER_ZIP_CODE"]
     weather_country: str = os.environ["WEATHER_COUNTRY"]
 
-    return requests.get("https://api.openweathermap.org/data/2.5/weather?zip=11230,us&appid=8d96edab6c640b9d6b6f97ce1bad77f6").json()
+    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={weather_zip_code},{weather_country}&appid={api_key}").json()
