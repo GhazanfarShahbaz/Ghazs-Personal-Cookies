940a32c8fccc0c75ec2236878f594d0d3a6aac87
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2021-10-10 05:26:57 +0000

    folder restructure

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,8 @@ from werkzeug.middleware.dispatcher import \
     DispatcherMiddleware  # use to combine each Flask app into a larger one that is dispatched based on prefix
 from werkzeug.serving import run_simple  # werkzeug development server
 
-from personal_website.requests import app as personal_website_app
-from tool_repository.requests import app as tool_app
-
+from apps.personal_website.requests import app as personal_website_app
+from apps.tool_repository.requests import app as tool_app
 
 def set_up_application():
     application: DispatcherMiddleware = DispatcherMiddleware(personal_website_app, {"/tools": tool_app})
