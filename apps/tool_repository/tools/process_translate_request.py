128a41ecb3e8988a9e872d38c72321fd3fdf3d06
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-04-17 04:43:03 +0000

    edit help

--- apps/tool_repository/tools/process_translate_request.py
+++ apps/tool_repository/tools/process_translate_request.py
@@ -1,31 +1,8 @@
 from google.cloud import translate_v2 as translate
 
-from translate_utils import get_languages_and_abbreviations
+from translate_utils import translate_line
 
 import six
 
-abbreviations, language_to_abbreviations = get_languages_and_abbreviations()
-
-def correct_language(language: str, response: dict) -> str:
-    if language and not language in abbreviations:
-        if language in language_to_abbreviations.keys():
-            return language_to_abbreviations[language]
-        else:
-            return None
-    elif not language:
-        return None
-
-    return language
-
-def get_line_translation(text: str, source: str, target: str) -> dict:
-    response: dict = {}
-    text = text.strip()
-    source = correct_language(source.strip(), response)
-    target = correct_language(target.strip(), response)
-    
-    client = translate.Client()
-            
-    response["translationInformation"] = client.translate(text, source_language=source, target_language=target)
-
-    return response
-
+def process_translate(translate_form: dict) -> dict:
+    return translate_line(translate_form["text"], translate_form["source"], translate_form["target"])
--- apps/tool_repository/tools/translate_utils.py
+++ apps/tool_repository/tools/translate_utils.py
@@ -19,5 +19,30 @@ def get_languages_and_abbreviations() -> tuple:
     
     return abbreviations, languages_to_abbreviations
 
+abbreviations, language_to_abbreviations = get_languages_and_abbreviations()
+
+def correct_language(language: str, response: dict) -> str:
+    if language and not language in abbreviations:
+        if language in language_to_abbreviations.keys():
+            return language_to_abbreviations[language]
+        else:
+            return None
+    elif not language:
+        return None
+
+    return language
+
+def translate_line(text: str, source: str, target: str) -> dict:
+    response: dict = {}
+    text = text.strip()
+    source = correct_language(source.strip(), response)
+    target = correct_language(target.strip(), response)
+    
+    client = translate.Client()
+            
+    response["translationInformation"] = client.translate(text, source_language=source, target_language=target)
+
+    return response
+
 
     
