8c77e6114068fcf0577405f7ae07b227413019a2
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-04-19 23:05:55 +0000

    sample env

--- apps/tool_repository/validate.py
+++
@@ -1,5 +0,0 @@
-def validate_user(username: str, password: str) -> bool:
-    if not username or not password:
-        return False 
-        
-    return username == "q_o1S6jV/Ttb8aq(a39aIFZCY;vA@" and password == "6&K5PO:zupdED{OmM])5fVPkPDqy8Yn"
