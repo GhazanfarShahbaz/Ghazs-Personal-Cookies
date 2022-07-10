08c75741514edd96ae5192136da6ceb50d7b0fce
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-07-10 21:35:34 +0000

    delete object

--- apps/tool_repository/tools/process_file_storage_requests.py
+++ apps/tool_repository/tools/process_file_storage_requests.py
@@ -1,13 +1,11 @@
-from distutils.command.upload import upload
 from typing import Dict
 
-from werkzeug import Response
+from file_storage_utils import upload_file, delete_file
 
-from file_storage_utils import upload_file, upload_file_object
 
-def process_upload_file_object(file, content_type) -> Dict[str, bool]:
-    
-    return {"success": upload_file_object(file, content_type)}
+def process_upload_file(file, content_type) -> Dict[str, str]:
+    return {"status": upload_file(file, content_type)}
 
-def process_upload_file(file_upload_form: Dict[str, str]) -> Dict[str, bool]:
-    return {"success": upload_file(file_upload_form["FileName"], file_upload_form["ObjectName"])}
+
+def process_delete_file(delete_file_form) -> Dict[str, str]:
+    return {"status": delete_file(delete_file_form["bucket"], delete_file_form["FilePath"])}
