4a710200f5982c10d21dcc023767ecbc30cc421e
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-07-30 23:30:51 +0000

    new tests

--- apps/tool_repository/tools/translate_utils.py
+++ apps/tool_repository/tools/translate_utils.py
@@ -2,24 +2,24 @@ from google.cloud import translate_v2 as translate
 
 from typing import Dict, Set, Tuple
 
-import six
 
-def get_languages_and_abbreviations() -> tuple:
+def get_languages_and_abbreviations() -> Tuple[Set[str], Dict[str, str]]:
     abbreviations: Set[str] = set()
     languages_to_abbreviations: Dict[str, str] = {}
-    
+
     client = translate.Client()
-    
+
     for language_dict in client.get_languages():
         abbreviation: str = language_dict['language']
-        
+
         abbreviations.add(abbreviation)
         languages_to_abbreviations[language_dict["name"]] = abbreviation
-        
-    
+
     return abbreviations, languages_to_abbreviations
 
+
 abbreviations, language_to_abbreviations = get_languages_and_abbreviations()
+
 
 def correct_language(language: str, response: dict) -> str:
     if language and not language in abbreviations:
@@ -32,17 +32,16 @@ def correct_language(language: str, response: dict) -> str:
 
     return language
 
+
 def translate_line(text: str, source: str, target: str) -> dict:
     response: dict = {}
     text = text.strip()
     source = correct_language(source.strip(), response)
     target = correct_language(target.strip(), response)
-    
+
     client = translate.Client()
-            
-    response["translationInformation"] = client.translate(text, source_language=source, target_language=target)
+
+    response["translationInformation"] = client.translate(
+        text, source_language=source, target_language=target)
 
     return response
-
-
-    
--- tests/test_tool_repository_requests.py
+++ tests/test_tool_repository_requests.py
@@ -16,118 +16,223 @@ db = firestore.client()
 users_ref = db.collection(os.environ["FIRESTORE_SERVER"])
 credentials = users_ref.document(os.environ["FIRESTORE_DOC_ID"]).get().to_dict()
 
-def test_get_login_one():
-    response = get_login(False)
-    assert response is None
+# def test_get_login_one():
+#     response = get_login(False)
+#     assert response is None
 
 
-def test_get_login_two():
+# def test_get_login_two():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = get_login(False)
+
+#     assert response is not None
+#     assert "username" in response
+#     assert type(response["username"]) is str
+#     assert "password" in response
+#     assert type(response["password"]) is str
+
+
+# def test_get_login_three():
+#     response = get_login(True)
+
+#     assert response is not None
+#     assert "username" in response
+#     assert type(response["username"]) is str
+#     assert "password" in response
+#     assert type(response["password"]) is str
+
+
+# def test_validate_user():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+#     response = validate_user(credentials["username"], credentials["password"])
+
+#     assert response is True
+
+
+# def test_validate_get_help_one():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/getHelp",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#             "command": "getTranslation"
+#         }
+#     )
+
+#     response_dict = json.loads(response.data.decode('UTF-8'))
+
+#     assert response.status_code == 200
+#     assert response_dict == {
+#         "Description": "Translates a piece of text based on a series of parameters",
+#         "RequestTemplate": {
+#             "username": "your username",
+#             "password": "your password",
+#             "translationForm": {
+#                 "text": "a required parameter, string type",
+#                 "source": "an optional parameter, string type",
+#                 "target": "an optional parameter, string type"
+#             }
+#         }
+#     }
+    
+# def test_validate_get_help_two():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/getHelp",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#             "command": "somethingFake"
+#         }
+#     )
+
+#     response_dict = json.loads(response.data.decode('UTF-8'))
+
+#     assert response.status_code == 200
+#     assert response_dict == {}
+
+
+# def test_validate_get_current_weather():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/getCurrentWeather",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#         }
+#     )
+
+#     response_dict = json.loads(response.data.decode('UTF-8'))
+
+#     assert response.status_code == 200
+#     assert "weather" in response_dict
+
+
+# def test_create_event():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/createEvent",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#             "eventForm": {
+#                 "StartDate": "08/27/2001 12:00 AM",
+#                 "EndDate": "08/27/2001 12:01 AM",
+#                 "Name": "TEST",
+#                 "Location": "TEST",
+#                 "Type": "TEST",
+#                 "Description": "TEST",
+#                 "ReccuranceType": ""
+#             }
+#         }
+#     )    
+    
+#     assert response.status_code == 200
+#     assert response.data == b"Success"
+
+
+# def test_get_event():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/getEvent",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#             "filterForm": {
+#                 "Name": "TEST",
+#             }
+#         }
+#     )   
+    
+#     response_dict = json.loads(response.data.decode('UTF-8'))
+ 
+#     print(response_dict)
+#     assert response.status_code == 200
+#     assert response_dict != {}
+
+
+# def test_delete_event():
+#     login_allow = users_ref.document('allow')
+
+#     login_allow.update({
+#         u'allow': True
+#     })
+
+#     response = app.test_client().post(
+#         "/deleteEvent",
+#         json={
+#             "username": credentials['username'],
+#             "password": credentials['password'],
+#             "deleteForm": {
+#                 "Name": "TEST",
+#             }
+#         }
+#     )    
+    
+#     assert response.status_code == 200
+#     assert response.data == b"Success"
+
+
+def test_upload_file():
     login_allow = users_ref.document('allow')
 
     login_allow.update({
         u'allow': True
     })
-
-    response = get_login(False)
-
-    assert response is not None
-    assert "username" in response
-    assert type(response["username"]) is str
-    assert "password" in response
-    assert type(response["password"]) is str
-
-
-def test_get_login_three():
-    response = get_login(True)
-
-    assert response is not None
-    assert "username" in response
-    assert type(response["username"]) is str
-    assert "password" in response
-    assert type(response["password"]) is str
-
-
-def test_validate_user():
-    login_allow = users_ref.document('allow')
-
-    login_allow.update({
-        u'allow': True
-    })
-    response = validate_user(credentials["username"], credentials["password"])
-
-    assert response is True
-
-
-def test_validate_get_help_one():
-    login_allow = users_ref.document('allow')
-
-    login_allow.update({
-        u'allow': True
-    })
-
-    response = app.test_client().post(
-        "/getHelp",
-        json={
-            "username": credentials['username'],
-            "password": credentials['password'],
-            "command": "getTranslation"
-        }
-    )
-
-    response_dict = json.loads(response.data.decode('UTF-8'))
-
-    assert response.status_code == 200
-    assert response_dict == {
-        "Description": "Translates a piece of text based on a series of parameters",
-        "RequestTemplate": {
-            "username": "your username",
-            "password": "your password",
-            "translationForm": {
-                "text": "a required parameter, string type",
-                "source": "an optional parameter, string type",
-                "target": "an optional parameter, string type"
-            }
-        }
+    
+    base_request = {
+        "username": credentials["username"],
+        "password": credentials["password"]
     }
     
-def test_validate_get_help_two():
-    login_allow = users_ref.document('allow')
-
-    login_allow.update({
-        u'allow': True
-    })
-
+    
     response = app.test_client().post(
-        "/getHelp",
-        json={
-            "username": credentials['username'],
-            "password": credentials['password'],
-            "command": "somethingFake"
+        "uploadFile",
+        data={
+            "file": open("/home/ghaz/flask_gateway/tests/test_file.txt", "rb"),
+            "json": (None, json.dumps(base_request), 'application/json')
         }
-    )
-
-    response_dict = json.loads(response.data.decode('UTF-8'))
-
+    )  
+    
+    # response_dict = json.loads(response.data.decode('UTF-8'))
+    
     assert response.status_code == 200
-    assert response_dict == {}
-
-
-def test_validate_get_current_weather():
-    login_allow = users_ref.document('allow')
-
-    login_allow.update({
-        u'allow': True
-    })
-
-    response = app.test_client().post(
-        "/getCurrentWeather",
-        json={
-            "username": credentials['username'],
-            "password": credentials['password'],
-        }
-    )
-
-    response_dict = json.loads(response.data.decode('UTF-8'))
-
-    assert response.status_code == 200
-    assert "weather" in response_dict
+    assert response.data != "Invalid"
+    # assert response_dict == {"status": "success"}
+    
+    
