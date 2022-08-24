f9de6ac7d5cc1f6aa4197957a47d0605dd238c68
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-08-24 22:19:06 +0000

    serve react app

--- apps/personal_website/requests.py
+++ apps/personal_website/requests.py
@@ -1,14 +1,15 @@
 import logging
 from flask import Flask 
-from flask import request, jsonify
+from flask import request, redirect, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='/home/ghaz/personal_website/build', template_folder='/home/ghaz/personal_website/build')
 logging.basicConfig(filename='logs/personal_website_requests.log', level=logging.DEBUG)
 
 @app.route("/")
 def home_route():
     app.logger.info(f'Someone accessed the website {request.remote_addr}')
-    return "This site is under maintenance"
+    # return redirect("http://www.ghazanfarshahbaz.dev", code=302)
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
