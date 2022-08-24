4d1fbd5ddf8da4ace3ded8c7668bff5204a18b85
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-08-24 01:56:41 +0000

    insert diagnostic logic

---
+++ apps/tool_repository/tools/endpoint_diagnostics.py
@@ -0,0 +1,35 @@
+from repository.endpoint_diagnostics import EndpointDiagnosticsRepository
+from repository.model import EndpointDiagnostics
+
+from typing import Dict 
+from datetime import datetime
+
+endpoint_dict: Dict[int, any] = {}
+current_index: int = 0
+
+def setup_endpoint_diagnostics(endpoint: str, request) -> int:
+    endpoint_diagnostics: Dict[str, any] = {
+        "Endpoint"  : endpoint,
+        "Request"   : request,
+        "Date"      : datetime.now(),
+    }
+    
+    endpoint_id: int = current_index
+    endpoint_dict[endpoint_id] = endpoint_diagnostics    
+    current_index += 1
+    
+    return endpoint_id
+    
+    
+def commit_endpoint_diagnostics(diagnostic_id: int, response, error = "") -> bool:
+    endpoint_diagnostics: Dict[str, any] = endpoint_dict[diagnostic_id]
+    
+    endpoint_diagnostics["Response"] = response 
+    endpoint_diagnostics["Error"] = error 
+    endpoint_diagnostics["Latency"] = datetime.now() - endpoint_diagnostics["Date"]
+    
+    EndpointDiagnosticsRepository().insert(EndpointDiagnostics(endpoint_diagnostics))
+    del endpoint_dict[diagnostic_id]
+    
+    
+    return True
