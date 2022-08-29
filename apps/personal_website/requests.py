54bb3b79f585bca154b0d437217fd7bcdd625813
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-08-29 04:05:51 +0000

    fix reloading problem

--- apps/personal_website/requests.py
+++ apps/personal_website/requests.py
@@ -1,15 +1,36 @@
 import logging
-from flask import Flask 
-from flask import request, redirect, send_from_directory
+from flask import Flask
+from flask import request, send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='/home/ghaz/personal_website/build', template_folder='/home/ghaz/personal_website/build')
-logging.basicConfig(filename='logs/personal_website_requests.log', level=logging.DEBUG)
+app = Flask(__name__, static_url_path='', static_folder='/home/ghaz/personal_website/build',
+            template_folder='/home/ghaz/personal_website/build')
+logging.basicConfig(
+    filename='logs/personal_website_requests.log', level=logging.DEBUG)
+
 
 @app.route("/")
 def home_route():
     app.logger.info(f'Someone accessed the website {request.remote_addr}')
-    # return redirect("http://www.ghazanfarshahbaz.dev", code=302)
     return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/projects")
+def projects_route():
+    app.logger.info(f'Someone accessed the website {request.remote_addr}')
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/skills")
+def skills_route():
+    app.logger.info(f'Someone accessed the website {request.remote_addr}')
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route("/education")
+def education_route():
+    app.logger.info(f'Someone accessed the website {request.remote_addr}')
+    return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
