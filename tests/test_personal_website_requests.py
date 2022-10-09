28841a7205ec61fc6d6b0d9b1dfe5ac4ad468ec0
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-09 07:42:26 +0000

    update test names

--- tests/test_personal_website_requests.py
+++ tests/test_personal_website_requests.py
@@ -10,35 +10,35 @@ import json
 import apps
 from apps.personal_website.requests import app
 
-def test_gethome():
+def test_get_home():
     response = app.test_client().get("/")
     
     assert response.status_code == 200
     
     
-def test_getprojects():
+def test_get_projects():
     response = app.test_client().get("/projects")
     
     assert response.status_code == 200
     
     
-def test_getskills():
+def test_get_skills():
     response = app.test_client().get("/skills")
     
     assert response.status_code == 200
     
-def test_getskills():
+def test_get_skills():
     response = app.test_client().get("/skills")
     
     assert response.status_code == 200
     
-def test_geteducation():
+def test_get_education():
     response = app.test_client().get("/education")
     
     assert response.status_code == 200
     
     
-def test_getresume():
+def test_get_resume():
     response = app.test_client().get("/resume")
     
     assert response.status_code == 200
