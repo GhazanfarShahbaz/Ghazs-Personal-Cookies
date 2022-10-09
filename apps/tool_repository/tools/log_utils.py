cfb8deca5be537e0d53a9fc2584edd0908567163
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-09 07:29:30 +0000

    update logging system

--- apps/tool_repository/tools/log_utils.py
+++ apps/tool_repository/tools/log_utils.py
@@ -1,27 +1,32 @@
 from typing import Dict, List
+from datetime import datetime
 
-PATH_TO_LOG_FILE: str = "/home/ghaz/flask_gateway/logs/personal_website_requests.log"
+PATH_TO_LOG_FILE: str = "/home/ghaz/flask_gateway/logs/"
 
 def split_up_log_file() -> Dict[str, List[str]]:
-    log_file: TextIOWrapper = open(PATH_TO_LOG_FILE, "r")
+    current_log_file: str = '{:%Y-%m-%d}.log'.format(datetime.now())
     
-    log_dictionary: Dict[str, List[str]] = {"": []}
-    previous_app: str = ""
+    log_file: TextIOWrapper = open(f"{PATH_TO_LOG_FILE}{current_log_file}", "r")
+    
+    log_dictionary: Dict[str, List[str]] = {"personal_website": []}
+    previous_app: str = "personal_website"
     
     for line in log_file:
-        split_colon: List[str] = line.split(":")
+        split_colon: List[str] = line.split("|")
 
         app: str = previous_app 
         
-        if split_colon[0] == "INFO":
-            split_dot: List[str] = split_colon[1].split(".")
-            if split_dot[0] == "apps":
-                app = split_dot[1]
+        if len(split_colon) >= 2 and split_colon[1].strip() == "INFO":
+            slash_count: List[str] = split_colon[3].count("/")
+            if slash_count <= 1:
+                app = "personal_website"
+            else:
+                app = "tools"  
                 
-                if not app in log_dictionary.keys():
-                    log_dictionary[app] = []
-                
-                previous_app = app
+            if not app in log_dictionary.keys():
+                log_dictionary[app] = []         
+                     
+            previous_app = app
             
         log_dictionary[app].append(line)
         
