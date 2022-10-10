b1c86eb9dae55786e3c83f9080970ceda4e1797f
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-10 03:30:43 +0000

    format tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import sys
 
+
 def pytest_configure():
     root_dir: str = "/home/ghaz/flask_gateway/"
     sys.path.append(root_dir)
-    
-    from generate_env import load_environment # pylint: disable=import-error, import-outside-toplevel
+
+    from generate_env import load_environment  # pylint: disable=import-error, import-outside-toplevel
     load_environment()
 
-    
     # load_environment()
