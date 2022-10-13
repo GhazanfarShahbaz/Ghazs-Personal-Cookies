72446192b8fc821b50fff123bdcd63a4e18ed75f
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-13 05:43:24 +0000

    fix logging mechanism

--- apps/tool_repository/endpoints.py
+++ apps/tool_repository/endpoints.py
@@ -32,11 +32,12 @@ app = Flask(__name__)
 logging.config.fileConfig('/home/ghaz/flask_gateway/logging.conf')
 app.logger = logging.getLogger('MainLogger')
 
-fh = logging.FileHandler('logs/{:%Y-%m-%d}.log'.format(datetime.now()))
-formatter = logging.Formatter(
-    '%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
-fh.setFormatter(formatter)
-app.logger.addHandler(fh)
+handler = logging.handlers.TimedRotatingFileHandler('logs/{:%Y-%m-%d}.log'.format(datetime.now()), when="midnight", interval=1)
+
+# fh = logging.FileHandler('logs/{:%Y-%m-%d}.log'.format(datetime.now()))
+formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
 
 APP_PATH: str = "/tools"
 
