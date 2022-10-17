8be735bd6f27ab69834f63dd25ee707d01cf5e29
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-17 06:58:59 +0000

    add comments

--- script/install-hooks.bash
+++ script/install-hooks.bash
@@ -6,3 +6,6 @@ echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../script/pre-commit.bash $GIT_DIR/hooks/pre-commit
 echo "Done"!
+
+# https://www.andrewcbancroft.com/blog/musings/make-bash-script-executable/
+# https://medium.com/@yagizcemberci/automate-unit-tests-with-git-hooks-e25e8b564c92
