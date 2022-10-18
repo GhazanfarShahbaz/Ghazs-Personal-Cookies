0592a56eda58946a3c6c7ccd138e8613aa162f9d
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-18 20:01:47 +0000

    test get weather

---
+++ tests/unit/test_get_weather.py
@@ -0,0 +1,9 @@
+from apps.tool_repository.tools.process_weather_requests import get_weather  # pylint: disable=import-error, useless-option-value, nknown-option-value, unrecognized-option
+
+import pytest 
+
+def test_get_weather():
+    weather_data = get_weather()
+    
+    assert type(weather_data) is dict
+    assert weather_data != {}
