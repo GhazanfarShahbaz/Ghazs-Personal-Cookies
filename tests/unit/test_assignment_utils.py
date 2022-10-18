ab4a51a51f5281001d465f8e9386b872a4a13dd9
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-18 05:45:35 +0000

    fix pytest

--- tests/unit/test_assignment_utils.py
+++ tests/unit/test_assignment_utils.py
@@ -7,42 +7,35 @@ from datetime import datetime
 from typing import List
 
 @pytest.fixture
-def my_assignments():
-    return [
-        Assignment({
+def assignment_one_dict():
+    return {
             "ClassId": 1,
+            "AssignmentId": None,
             "Grade": 100,
             "DateAssigned": datetime(2022, 10, 18, 2, 30),
             "DateDue": datetime(2022, 10, 19, 11, 59),
             "Submitted": True   
-        }),
-        Assignment({
+        }
+    
+@pytest.fixture
+def assignment_two_dict():
+    return {
             "ClassId": 2,
+            "AssignmentId": None,
             "Grade": 100,
             "DateAssigned": datetime(2022, 9, 18, 2, 30),
             "DateDue": datetime(2022, 9, 19, 11, 59),
-            "Submitted": False   
-        })
-    ]
+            "Submitted": False  
+        }
+
+@pytest.fixture
+def my_assignments(assignment_one_dict, assignment_two_dict):
+    return [Assignment(assignment_one_dict), Assignment(assignment_two_dict)]
     
-def test_type_list_to_event_dict_list(my_assignments: List[Assignment]):
+    
+def test_type_list_to_event_dict_list(my_assignments: List[Assignment], assignment_one_dict, assignment_two_dict):
     assignment_list = assignment_type_list_to_event_dict_list(my_assignments)
     
     assert len(assignment_list) == len(my_assignments)
-    assert assignment_list[0] == {
-            "ClassId": 1,
-            "AssignmentId": None,
-            "Grade": 100,
-            "DateAssigned": datetime(2022, 10, 18, 2, 30),
-            "DateDue": datetime(2022, 10, 19, 11, 59),
-            "Submitted": True   
-        }
-    
-    assert assignment_list[1] == {
-            "ClassId": 2,
-            "AssignmentId": None,
-            "Grade": 100,
-            "DateAssigned": datetime(2022, 9, 18, 2, 30),
-            "DateDue": datetime(2022, 9, 19, 11, 59),
-            "Submitted": False   
-        }
+    assert assignment_list[0] == assignment_one_dict
+    assert assignment_list[1] == assignment_two_dict
