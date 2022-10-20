9807f2ab1b7d4bb70db18913f94bd05562282508
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-20 03:00:41 +0000

    submodules

--- apps/personal_website/requests.py
+++ apps/personal_website/requests.py
@@ -6,8 +6,8 @@ from flask import request, send_from_directory
 app = Flask(
     __name__,
     static_url_path='',
-    static_folder='./personal_website/build',
-    template_folder='./personal_website/build'
+    static_folder='./personal_website_files/build',
+    template_folder='./personal_website_files/build'
 )
 
 logging.config.fileConfig('/home/ghaz/flask_gateway/logging.conf')
