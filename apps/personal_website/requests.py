de5f26c697716f12dcfa111330c7a83921ccbce1
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-20 03:39:33 +0000

    dynamic url

--- apps/personal_website/requests.py
+++ apps/personal_website/requests.py
@@ -1,8 +1,9 @@
 import logging.config
 
-from flask import Flask
+from flask import Flask, abort
 from flask import request, send_from_directory
 
+# Create flask app. Static and template folder point to personal website react build files
 app = Flask(
     __name__,
     static_url_path='',
@@ -10,54 +11,42 @@ app = Flask(
     template_folder='/home/ghaz/PersonalWebsite/build'
 )
 
+# Set logger config
 logging.config.fileConfig('/home/ghaz/flask_gateway/logging.conf')
 app.logger = logging.getLogger('MainLogger')
 
+# Handler for log file. So log files are rotated everyday
+# handler = logging.handlers.TimedRotatingFileHandler(
+# 'logs/app.log', when="midnight")
+# handler.prefix = "%Y%m%d"
 
-handler = logging.handlers.TimedRotatingFileHandler(
-'logs/app.log', when="midnight")
+# # Formatter for log file. Log files will be formatted in the format specified below
+# formatter = logging.Formatter('%(asctime)s | %(pathname)s | %(levelname)-8s | %(filename)s-%(funcName)s-%(lineno)04d | %(message)s')
+# handler.setFormatter(formatter)
 
-handler.prefix = "%Y%m%d"
+# # Add handler to the app logger
+# app.logger.addHandler(handler)
 
-formatter = logging.Formatter('%(asctime)s | %(pathname)s | %(levelname)-8s | %(filename)s-%(funcName)s-%(lineno)04d | %(message)s')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
+def log_endpoint(request: dict):
+    """
+        Log endpoint with ip address and path accessed
+    """
+    app.logger.info(f'Someone accessed the website {request.remote_addr} {request.path}')
 
 
 @app.route("/")
 def home_route():
-    app.logger.info(
-        f'Someone accessed the website {request.remote_addr} {request.path}')
+    log_endpoint(request)
     return send_from_directory(app.static_folder, 'index.html')
 
-
-@app.route("/projects")
-def projects_route():
-    app.logger.info(
-        f'Someone accessed the website {request.remote_addr} {request.path}')
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-@app.route("/skills")
-def skills_route():
-    app.logger.info(
-        f'Someone accessed the website {request.remote_addr} {request.path}')
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-@app.route("/education")
-def education_route():
-    app.logger.info(
-        f'Someone accessed the website {request.remote_addr} {request.path}')
-    return send_from_directory(app.static_folder, 'index.html')
-
-
-@app.route("/resume")
-def resume_route():
-    app.logger.info(
-        f'Someone accessed the website {request.remote_addr} {request.path}')
-    return send_from_directory(app.static_folder, 'index.html')
-
+@app.route('/<path>')
+def render_path(path: str):
+    log_endpoint(request)
+    
+    if path in {"/", "projects", "skills", "education", "resume"}:
+        return send_from_directory(app.static_folder, 'index.html')
+    else:
+        abort(404)
 
 if __name__ == "__main__":
     app.run(debug=True)
