b42b32aef81163d164b90dd0c803c06a2a4b5728
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-10-21 05:00:42 +0000

    add test robots.txt

--- tests/integration/test_personal_website_requests.py
+++ tests/integration/test_personal_website_requests.py
@@ -29,3 +29,9 @@ def test_get_resume():
     response = app.test_client().get("/resume")
 
     assert response.status_code == 200
+    
+
+def test_get_robots_txt():
+    response = app.test_client().get("/robots.txt")
+
+    assert response.status_code == 200
