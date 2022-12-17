eedfd076c0f686aeb44a381c2f2ee7ab1e7f5769
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-12-17 11:57:36 +0000

    class_utils documentation

--- apps/tool_repository/tools/class_utils.py
+++ apps/tool_repository/tools/class_utils.py
@@ -3,5 +3,11 @@ from typing import List
 from repository.model import Class
 
 
-def class_type_list_to_event_dict_list(class_list: List[dict]) -> List[Class]:
+def class_type_list_to_event_dict_list(class_list: List[Class]) -> List[dict]:
+    """
+    Converts class type list to an event dictionary for responses
+
+    Returns:
+        List[dict]: A list of assignmnents in dictionary form
+    """
     return [_class.to_dict() for _class in class_list]
