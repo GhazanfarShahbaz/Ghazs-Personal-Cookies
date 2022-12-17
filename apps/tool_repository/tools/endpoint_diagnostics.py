b23d56e93ab45e68479545d0956b3cb72198f1b2
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-12-17 12:11:19 +0000

    endpoint_diagnostics global variables

--- apps/tool_repository/tools/endpoint_diagnostics.py
+++ apps/tool_repository/tools/endpoint_diagnostics.py
@@ -4,8 +4,8 @@ from repository.model import EndpointDiagnostics
 from datetime import datetime
 from typing import Dict, List
 
-endpoint_dict: Dict[int, any] = {}
-current_index: int = 0
+ENDPOINT_DICT: Dict[int, any] = {}
+CURRENT_INDEX: int = 0
 
 
 def setup_endpoint_diagnostics(endpoint: str, request) -> int:
@@ -15,17 +15,17 @@ def setup_endpoint_diagnostics(endpoint: str, request) -> int:
         "Date": datetime.now(),
     }
 
-    global current_index, endpoint_dict
-    endpoint_id: int = current_index
-    endpoint_dict[endpoint_id] = endpoint_diagnostics
-    current_index += 1
+    global CURRENT_INDEX, ENDPOINT_DICT
+    endpoint_id: int = CURRENT_INDEX
+    ENDPOINT_DICT[endpoint_id] = endpoint_diagnostics
+    CURRENT_INDEX += 1
 
     return endpoint_id
 
 
 def commit_endpoint_diagnostics(diagnostic_id: int, response, error="") -> bool:
-    global endpoint_dict
-    endpoint_diagnostics: Dict[str, any] = endpoint_dict[diagnostic_id]
+    global ENDPOINT_DICT
+    endpoint_diagnostics: Dict[str, any] = ENDPOINT_DICT[diagnostic_id]
 
     endpoint_diagnostics["Response"] = response
     endpoint_diagnostics["Error"] = error
@@ -35,7 +35,7 @@ def commit_endpoint_diagnostics(diagnostic_id: int, response, error="") -> bool:
     with EndpointDiagnosticsRepository() as repository:
         repository.insert(EndpointDiagnostics(endpoint_diagnostics))
     
-    del endpoint_dict[diagnostic_id]
+    del ENDPOINT_DICT[diagnostic_id]
 
     return True
 
