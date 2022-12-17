7def8654fbba45a2fc71183e7247c28785baa629
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-12-17 12:15:34 +0000

    endpoint_diagnostics documentation

--- apps/tool_repository/tools/endpoint_diagnostics.py
+++ apps/tool_repository/tools/endpoint_diagnostics.py
@@ -8,7 +8,14 @@ ENDPOINT_DICT: Dict[int, any] = {}
 CURRENT_INDEX: int = 0
 
 
-def setup_endpoint_diagnostics(endpoint: str, request) -> int:
+def setup_endpoint_diagnostics(endpoint: str, request: dict) -> int:
+    """
+        Setups up diganostics for an endpoint using a string and users request form
+        
+        Return: 
+            int: An id for the current diagnostic
+    """
+    
     endpoint_diagnostics: Dict[str, any] = {
         "Endpoint": endpoint,
         "Request": request,
@@ -23,7 +30,14 @@ def setup_endpoint_diagnostics(endpoint: str, request) -> int:
     return endpoint_id
 
 
-def commit_endpoint_diagnostics(diagnostic_id: int, response, error="") -> bool:
+def commit_endpoint_diagnostics(diagnostic_id: int, response: dict, error="") -> bool:
+    """
+        Pushes endpoint diagnostics with the new feature diagnostic values
+        
+        Return: 
+            bool: True if successful
+    """
+    
     global ENDPOINT_DICT
     endpoint_diagnostics: Dict[str, any] = ENDPOINT_DICT[diagnostic_id]
 
@@ -40,5 +54,11 @@ def commit_endpoint_diagnostics(diagnostic_id: int, response, error="") -> bool:
     return True
 
 
-def diagnostics_type_list_to_diagnostic_dict_list(diagnostic_list: List[EndpointDiagnostics]):
+def diagnostics_type_list_to_diagnostic_dict_list(diagnostic_list: List[EndpointDiagnostics]) -> List[dict]:
+    """
+    Converts diagnostics type list to an diagnostics dictionary for responses
+
+    Returns:
+        List[dict]: A list of diagnostics in dictionary form
+    """
     return [diagnostic.to_dict() for diagnostic in diagnostic_list]
