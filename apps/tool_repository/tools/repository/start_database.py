da71ca882c77bb44ecd004f425bae9386d69104e
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-05-24 15:30:16 +0000

    start database documentation

--- apps/tool_repository/tools/repository/start_database.py
+++ apps/tool_repository/tools/repository/start_database.py
@@ -5,8 +5,16 @@ from os import system
 
 # activate /env/bin/source
 
-
 def check_database_status() -> bool:
+    """
+    Checks the status of the MySQL database.
+
+    This function connects to the MySQL database and returns True if the connection is successful,
+    and False if there is an error.
+
+    Returns:
+        A boolean value indicating if the connection was successful (True) or not (False).
+    """
     try:
         connection = mysql.connector.connect(
             host="localhost",
@@ -21,10 +29,26 @@ def check_database_status() -> bool:
         return False
 
 
-def start_myql() -> None:
+def start_mysql() -> None:
+    """
+    Starts the MySQL service.
+
+    This function starts the MySQL service if it is not already running.
+
+    Returns:
+        None
+    """
     system("brew services start mysql")
     time.sleep(5)
 
 
-def stop_myql() -> None:
+def stop_mysql() -> None:
+    """
+    Stops the MySQL service.
+
+    This function stops the MySQL service if it is currently running.
+
+    Returns:
+        None
+    """
     system("brew services stop mysql")
