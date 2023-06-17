da02ad52be5065f3de40b4fbf6c4c006cf0183fc
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-06-17 18:55:28 +0000

    Initialize knowledge graph

---
+++ apps/knowledge_graph/__init__.py
@@ -0,0 +1,6 @@
+import sys
+from os import path 
+
+# if this filei s called from a parent directory the tools.... path is needed this allows all modules to be called from this directory
+current = path.dirname(path.realpath(__file__))
+sys.path.append(current)
