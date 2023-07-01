6b275b8a57152d671725cfdfba46680c9ea15f55
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-01 18:34:06 +0000

    test endpoint in knowledge_graph application, update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ def pytest_configure():
     root_dir: str = "/home/ghaz/flask_gateway/"
     sys.path.append(root_dir)
 
-    from generate_env import (
+    from generate_env import (  # pylint: disable=import-error, import-outside-toplevel
         load_environment,
-    )  # pylint: disable=import-error, import-outside-toplevel
+    )
 
     load_environment()
