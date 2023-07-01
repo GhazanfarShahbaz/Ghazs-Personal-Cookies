3812874df51b2748978065acffcdc3c647364db9
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-01 18:01:09 +0000

    setup knowleadge graph application

--- apps/knowledge_graph/app.py
+++ apps/knowledge_graph/app.py
@@ -1,7 +1,68 @@
-from flask import Flask
-import json
+"""
+file_name: app.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/01/2023
+Description: Flask app for the knowledge_graph application.
+"""
 
-app = Flask(__name__)
+from json import load
+from pathlib import Path
+from os.path import join
+
+from flask import Flask
+from flask_cors import CORS
+
+from apps.knowledge_graph.utils.build_knowledge_graph import (  # pylint: disable=import-error
+    create_and_save_graph,
+)
+
+app: Flask = Flask(
+    __name__,
+)
+CORS(app) # allows for cross-origin requests
+
+# set app variables
+app.config["data_directory"] = join(Path(__file__).resolve().parent, "data")
+
+
+def load_and_return_graph():
+    """
+    Loads and returns the force-directed graph data from a JSON file.
+
+    Returns:
+        The loaded graph data.
+    """
+
+    with open(
+        join(app.config["data_directory"], "forceGraph.json"),
+        encoding="utf-8",
+    ) as data_file:
+        return load(data_file)
+
+
+@app.before_first_request
+def update_force_graph():
+    """
+    Updates the force-directed graph by creating and saving the graph data to a JSON file.
+
+    Returns:
+        None
+    """
+
+    create_and_save_graph(join(app.config["data_directory"], "forceGraph.json"))
+
+
+@app.route("/", methods=["GET"])
+def home_route():
+    """
+    Returns the loaded force-directed graph data.
+
+    Returns:
+        The loaded graph data.
+    """
+
+    return load_and_return_graph()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
