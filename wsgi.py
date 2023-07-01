da35da59ea043f28281df78cde6ee4ddad8629e7
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-01 18:01:38 +0000

    add knowledge graph application to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from apps.personal_website.requests import app as personal_website_app
 from apps.tool_repository.endpoints import app as tool_app
 from apps.projects.the_mouseion.app import app as mouseion_app
+from apps.knowledge_graph.app import app as knowledge_graph_app
 
 # use to combine each Flask app into a larger one that is dispatched based on prefix
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
@@ -22,7 +23,11 @@ def set_up_application() -> DispatcherMiddleware:
     """
 
     application: DispatcherMiddleware = DispatcherMiddleware(
-        personal_website_app, {"/tools": tool_app, "/the_mouseion": mouseion_app}
+        personal_website_app, {
+            "/tools": tool_app, 
+            "/the_mouseion": mouseion_app,
+            "/knowledge_graph": knowledge_graph_app
+        }
     )
 
     return application
