29d4bb30c0df2f655a723c5f6e2696f2d79971e9
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 02:09:33 +0000

    add tests for knowledge graph

--- tests/integration/test_knowledge_graph_requests.py
+++ tests/integration/test_knowledge_graph_requests.py
@@ -2,10 +2,11 @@ import pytest
 
 from json import loads
 
-from apps.knowledge_graph.app import app #pylint: disable=import-error
+from apps.knowledge_graph.app import app  # pylint: disable=import-error
+
 
 def test_get_base_endpoint():
     response = app.test_client().get("/")
-    
+
     assert response.status_code == 200
     assert loads(response.data.decode("UTF-8")) is not None
