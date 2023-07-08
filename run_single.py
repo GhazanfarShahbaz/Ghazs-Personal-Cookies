7bf4531577dabeaea8ea559cf8d514d5dc57c976
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 02:36:49 +0000

    update run_single to conform to pylint

--- run_single.py
+++ run_single.py
@@ -1,12 +1,24 @@
+"""
+file_name = run_single.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/07/2023
+Description: A file used to run a single flask application alone. This is used for debugging.
+"""
+
+import sys
+
+from typing import Dict
+
+from stringcolor import cs
+
 from apps.personal_website.requests import app as personal_website_app
 from apps.tool_repository.endpoints import app as tool_app
 from apps.knowledge_graph.app import app as knowledge_graph_app
 from apps.projects.the_mouseion.app import app as mouseion_app
 
-from typing import Dict
-from stringcolor import cs
 
-# Define a dictionary of Flask apps, where each key is an app number and each value is the corresponding app dictionary containing the applications name and module
+# Define a dictionary of Flask apps. Each key is an app number and
+# each value is the corresponding app dictionary containing the applications name and module
 app_list: Dict[int, dict] = {
     1: {"app_name": "Personal Website", "app_module": personal_website_app},
     2: {"app_name": "Tools Application", "app_module": tool_app},
@@ -33,10 +45,10 @@ try:
     app_number = int(app_number_temp)
 except ValueError:
     print(cs("\nThis is not a number, exiting script.", "red"))
-    exit()
+    sys.exit()
 
 # If the app number is in the app_list dictionary, run the corresponding app
-if app_number in app_list.keys():
+if app_number in app_list:
     app_list[app_number]["app_module"].run()
 # If the app number is not in the app_list dictionary, print an error message
 else:
