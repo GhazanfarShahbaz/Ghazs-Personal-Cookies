afff546318d571a7180b90c008433840941a95a3
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 04:22:30 +0000

    add module docstring

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,3 +1,13 @@
+"""
+file_name = __init__.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/08/2023
+Description: A file that loads environment upon importing anything in the apps
+directory.
+Edit Log:
+07/08/2023
+    - Added module docstring
+"""
 from generate_env import load_environment
 
 load_environment()
