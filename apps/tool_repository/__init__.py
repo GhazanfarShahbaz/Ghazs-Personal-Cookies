c1671de876ef624dd637bbb7128d0a08feb46732
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 02:07:24 +0000

    update __init__.py files

--- apps/tool_repository/__init__.py
+++ apps/tool_repository/__init__.py
@@ -1,10 +1,10 @@
-import sys
-from os import path
+# import sys
+# from os import path
 
-# if this filei s called from a parent directory the tools.... path is needed this allows all modules to be called from this directory
-current = path.dirname(path.realpath(__file__))
-sys.path.append(current)
+# # if this filei s called from a parent directory the tools.... path is needed this allows all modules to be called from this directory
+# # current = path.dirname(path.realpath(__file__))
+# # sys.path.append(current)
 
 
-import tools
-import response_processing
+# # import tools
+# # import response_processing
