d08ecfc0237d95101350fd6c3f91f1d0977087ec
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 02:29:54 +0000

    conform to pylint wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,18 @@
+"""
+file_name = wsgi.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/07/2023
+Description: A file used to combine flask applications and server them as one wsgi applicaton.
+"""
+
+# use to combine each Flask app into a larger one that is dispatched based on prefix
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 from apps.personal_website.requests import app as personal_website_app
 from apps.tool_repository.endpoints import app as tool_app
 from apps.knowledge_graph.app import app as knowledge_graph_app
 from apps.projects.the_mouseion.app import app as mouseion_app
 
-# use to combine each Flask app into a larger one that is dispatched based on prefix
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
 def set_up_application() -> DispatcherMiddleware:
@@ -12,7 +20,8 @@ def set_up_application() -> DispatcherMiddleware:
     Creates a dispatcher middleware that combines multiple Flask applications.
 
     This function creates a Flask dispatcher middleware that combines multiple Flask applications,
-    including `personal_website_app`, `tool_app`, and `mouseion_app`, and returns the resulting middleware.
+    including `personal_website_app`, `tool_app`, and `mouseion_app`, and returns the resulting 
+    middleware.
     The resulting middleware can be run like a Flask application.
 
     Returns:
