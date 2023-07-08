680c713bbed9451e63993ab1279915f24422947a
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-08 02:10:48 +0000

    clean up wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from apps.personal_website.requests import app as personal_website_app
 from apps.tool_repository.endpoints import app as tool_app
-from apps.projects.the_mouseion.app import app as mouseion_app
 from apps.knowledge_graph.app import app as knowledge_graph_app
+from apps.projects.the_mouseion.app import app as mouseion_app
 
 # use to combine each Flask app into a larger one that is dispatched based on prefix
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
@@ -23,11 +23,12 @@ def set_up_application() -> DispatcherMiddleware:
     """
 
     application: DispatcherMiddleware = DispatcherMiddleware(
-        personal_website_app, {
-            "/tools": tool_app, 
+        personal_website_app,
+        {
+            "/tools": tool_app,
             "/the_mouseion": mouseion_app,
-            "/knowledge_graph": knowledge_graph_app
-        }
+            "/knowledge_graph": knowledge_graph_app,
+        },
     )
 
     return application
