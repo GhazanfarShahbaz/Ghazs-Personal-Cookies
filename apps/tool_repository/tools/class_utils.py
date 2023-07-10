4037fc1b6d383e802933df7212a00872b0378064
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-10 02:22:27 +0000

    conform to pylint conventions class_utils.py

--- apps/tool_repository/tools/class_utils.py
+++ apps/tool_repository/tools/class_utils.py
@@ -1,3 +1,13 @@
+"""
+file_name = class_utils.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/09/2023
+Description: A module used for database class objects. 
+Edit Log:
+07/09/2023
+-   Conformed to pylint conventions.
+"""
+
 from typing import List
 
 from apps.tool_repository.tools.repository.models.class_model import Class
