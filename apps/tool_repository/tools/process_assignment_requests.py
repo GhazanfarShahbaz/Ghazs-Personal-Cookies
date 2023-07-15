c8f548c5110c5dea846301979dd067217ac9cb2e
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-15 02:23:29 +0000

    format process_assignment_requests based on pylint

--- apps/tool_repository/tools/process_assignment_requests.py
+++ apps/tool_repository/tools/process_assignment_requests.py
@@ -1,3 +1,13 @@
+"""
+file_name = process_assignment_requests.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/14/2023
+Description: A module used for storing and retrieving files from s3 buckets. 
+Edit Log:
+07/09/2023
+-   Conformed to pylint conventions.
+"""
+
 from typing import List
 from apps.tool_repository.tools.assignment_utils import (
     assignment_type_list_to_event_dict_list,
@@ -11,8 +21,8 @@ def process_create_assignment(assignment_data: dict) -> None:
     """
     Process a request to create a new assignment.
 
-    This function takes a dictionary representing assignment data from a request and inserts the data into the AssignmentRepository
-    to create the new assignment.
+    This function takes a dictionary representing assignment data from a request and 
+    inserts the data into the AssignmentRepository to create the new assignment.
 
     Args:
         assignment_data: A dictionary representing the assignment data from a request.
@@ -28,12 +38,12 @@ def process_update_assignment(update_form: dict) -> None:
     """
     Process a request to update an existing assignment.
 
-    This function takes a dictionary representing update data from a request and uses the data to update an existing assignment
-    in the AssignmentRepository.
+    This function takes a dictionary representing update data from a request and uses the
+    data to update an existing assignment in the AssignmentRepository.
 
     Args:
-        update_form: A dictionary representing the update data from a request. The required fields are "AssignmentId" and
-        "UpdateParams".
+        update_form: A dictionary representing the update data from a request. The required 
+        fields are "AssignmentId" and "UpdateParams".
 
     Returns:
         None. The function does not return anything.
@@ -48,16 +58,17 @@ def process_get_assignment_request(filter_form: dict) -> List[dict]:
     """
     Process a request to get a list of assignments.
 
-    This function takes a dictionary representing filter data from a request and uses the data to retrieve a list of
-    assignments from the AssignmentRepository.
+    This function takes a dictionary representing filter data from a request and uses the data 
+    to retrieve a list of assignments from the AssignmentRepository.
 
     Args:
-        filter_form: A dictionary representing the filter data from a request. The optional fields are "UserId", "ClassId",
-        "AssignmentId", "StartDate", "EndDate", and "Status".
+        filter_form: A dictionary representing the filter data from a request. The optional fields 
+        are "UserId", "ClassId", "AssignmentId", "StartDate", "EndDate", and "Status".
 
     Returns:
-        A list of dictionaries representing the retrieved assignments. Each dictionary contains the fields "AssignmentId",
-        "UserId", "ClassId", "StartDate", "EndDate", and "Status".
+        A list of dictionaries representing the retrieved assignments. 
+        Each dictionary contains the fields "AssignmentId", "UserId", "ClassId", "StartDate", 
+        "EndDate", and "Status".
     """
 
     assignment_list: List[Assignment] = AssignmentRepository().get(filter_form)
@@ -69,12 +80,12 @@ def process_delete_assignment_request(delete_form: dict) -> None:
     """
     Process a request to delete an assignment.
 
-    This function takes a dictionary representing delete data from a request and uses the data to delete an existing assignment
-    from the AssignmentRepository.
+    This function takes a dictionary representing delete data from a request and uses the data to 
+    delete an existing assignment from the AssignmentRepository.
 
     Args:
-        delete_form: A dictionary representing the delete data from a request. The required fields are "AssignmentId", "UserId",
-        and "ClassId".
+        delete_form: A dictionary representing the delete data from a request. The required fields 
+        are "AssignmentId", "UserId", and "ClassId".
 
     Returns:
         None. The function does not return anything.
