dcd1d33574e8545623fa3cc05562b3d0a6ef1cff
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-15 02:56:37 +0000

    process log requests format using pylint

--- apps/tool_repository/tools/process_log_requests.py
+++ apps/tool_repository/tools/process_log_requests.py
@@ -1,13 +1,24 @@
-from apps.tool_repository.tools.log_utils import split_up_log_file
+"""
+file_name = process_log_requests.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/14/2023
+Description: A module used to process log requests.
+Edit Log:
+07/14/2023
+-   Conformed to pylint conventions.
+"""
+
 from typing import Dict, List
+
+from apps.tool_repository.tools.log_utils import split_up_log_file
 
 
 def process_get_logs() -> Dict[str, List[str]]:
     """
     Processes a request to retrieve log files.
 
-    This function calls the `split_up_log_file` function to retrieve log files and returns them as a dictionary containing the
-    file names and their contents.
+    This function calls the `split_up_log_file` function to retrieve log files and returns 
+    them as a dictionary containing the file names and their contents.
 
     Returns:
         A dictionary containing the file names and their contents as a list of strings.
