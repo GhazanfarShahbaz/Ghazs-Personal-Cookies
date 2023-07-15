7c2028db4ec6e8bd71c6ce797590c4b7951e1601
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-15 03:03:27 +0000

    format process translate request based on pylint

--- apps/tool_repository/tools/process_translate_request.py
+++ apps/tool_repository/tools/process_translate_request.py
@@ -1,18 +1,27 @@
-from google.cloud import translate_v2 as translate
+"""
+file_name = process_translate_requests.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/14/2023
+Description: A module used to translate requests.
+Edit Log:
+07/14/2023
+-   Conformed to pylint conventions.
+"""
 
 from apps.tool_repository.tools.translate_utils import translate_line
-
 
 def process_translate(translate_form: dict) -> dict:
     """
     Processes a translation request.
 
-    This function takes a dictionary `translate_form` containing the text to be translated, the source language, and the target
-    language. It then calls the `translate_line` function with these parameters to perform the translation and returns a
-    dictionary containing the translated text.
+    This function takes a dictionary `translate_form` containing the text to be translated,
+    the source language, and the target language. 
+    It then calls the `translate_line` function with these parameters to perform the translation
+    and returns a dictionary containing the translated text.
 
     Args:
-        translate_form: A dictionary containing the text to be translated, the source language, and the target language.
+        translate_form: A dictionary containing the text to be translated, the source language,
+        and the target language.
 
     Returns:
         A dictionary containing the translated text.
