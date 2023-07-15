4918ecded22efe4204b5dbd33c0b455b9f8618e2
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-15 03:06:36 +0000

    process weather requests format using pylint

--- apps/tool_repository/tools/process_weather_requests.py
+++ apps/tool_repository/tools/process_weather_requests.py
@@ -1,15 +1,28 @@
-import requests
-from dotenv import load_dotenv
+"""
+file_name = process_weather_requests.py
+Creator: Ghazanfar Shahbaz
+Last Updated: 07/14/2023
+Description: A module used to process weather requests.
+Edit Log:
+07/14/2023
+-   Conformed to pylint conventions.
+-   Added timeout to get weather request.
+"""
+
 from os import environ
+
+from requests import get
 
 
 def get_weather() -> dict:
     """
     Retrieves weather information from an API.
 
-    This function retrieves weather information from an API using the OpenWeatherMap API. The function uses environment
-    variables to retrieve the API key, ZIP code, and country code. The function then sends a request to the OpenWeatherMap API
-    to retrieve the weather information for the specified location and returns it as a dictionary.
+    This function retrieves weather information from an API using the OpenWeatherMap API.
+    The function uses environment variables to retrieve the API key, ZIP code, and country 
+    code. 
+    The function then sends a request to the OpenWeatherMap API to retrieve the weather 
+    information for the specified location and returns it as a dictionary.
 
     Returns:
         A dictionary containing the weather information for the specified location.
@@ -19,6 +32,7 @@ def get_weather() -> dict:
     weather_zip_code: str = environ["WEATHER_ZIP_CODE"]
     weather_country: str = environ["WEATHER_COUNTRY"]
 
-    return requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?zip={weather_zip_code},{weather_country}&appid={api_key}"
+    return get(
+        f"https://api.openweathermap.org/data/2.5/weather?zip={weather_zip_code},{weather_country}&appid={api_key}", # pylint: disable=line-too-long
+        timeout=10
     ).json()
