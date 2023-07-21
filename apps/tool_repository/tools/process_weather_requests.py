60ca5fadafb30754cae33574a3dff4b117a3b134
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-07-21 00:36:26 +0000

    cache decorater for process weather reqeusts

--- apps/tool_repository/tools/process_weather_requests.py
+++ apps/tool_repository/tools/process_weather_requests.py
@@ -16,39 +16,9 @@ from os import environ
 from requests import get
 
 
-from apps.tool_repository.tools.redis_utils import RedisClient
+from apps.tool_repository.tools.redis_utils import cached
 
-def cache_weather_data(response: dict) -> None:
-    """
-    Caches the weather data for 5 minutes.
-
-    Args:
-        response (dict): The weather data to be cached.
-    """
-
-    with RedisClient() as client:
-        client.save("weather_data", response, 5)
-
-
-def get_cached_weather() -> dict:
-    """
-    Checks and returns cached weather data from redis.
-
-    Returns:
-        dict: Can be empty signifying no cached data or can be full of data
-    """
-
-    response: dict = {}
-
-    with RedisClient() as client:
-        try:
-            response = client.get("weather_data")
-        except KeyError as exception:
-            print("Weather data may have been expired", exception)
-
-    return response
-
-
+@cached("weather_data", 5)
 def get_weather() -> dict:
     """
     Retrieves weather information from an API.
@@ -67,14 +37,9 @@ def get_weather() -> dict:
     weather_zip_code: str = environ["WEATHER_ZIP_CODE"]
     weather_country: str = environ["WEATHER_COUNTRY"]
 
-    response: dict | None = get_cached_weather()
-
-    if not response:
-        response = get(
-            f"https://api.openweathermap.org/data/2.5/weather?zip={weather_zip_code},{weather_country}&appid={api_key}", # pylint: disable=line-too-long
-            timeout=10
-        ).json()
-
-        cache_weather_data(response)
+    response: dict = get(
+        f"https://api.openweathermap.org/data/2.5/weather?zip={weather_zip_code},{weather_country}&appid={api_key}", # pylint: disable=line-too-long
+        timeout=10
+    ).json()
 
     return response
