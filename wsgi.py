90c7e132381ad761905aa318c025032995df8189
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-08-05 22:32:53 +0000

    add coding questions to dispatcher

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,12 @@
 file_name = wsgi.py
 Creator: Ghazanfar Shahbaz
 Last Updated: 07/08/2023
-Description: A file used to combine flask applications and server them as one wsgi application.
+Description: A file used to combine flask applications and serve them as one wsgi application.
 Edit Log: 
 07/08/2023 
     - Refactored personal_website new flask file name
+08/05/2023
+    - Added questions application
 """
 
 # use to combine each Flask app into a larger one that is dispatched based on prefix
@@ -15,6 +17,7 @@ from apps.personal_website.app import app as personal_website_app
 from apps.tool_repository.app import app as tool_app
 from apps.knowledge_graph.app import app as knowledge_graph_app
 from apps.projects.the_mouseion.app import app as mouseion_app
+from apps.coding_questions.app import app as coding_questions_app
 
 
 
@@ -40,6 +43,7 @@ def set_up_application() -> DispatcherMiddleware:
             "/tools": tool_app,
             "/the_mouseion": mouseion_app,
             "/knowledge_graph": knowledge_graph_app,
+            "/coding_questions": coding_questions_app,
         },
     )
 
