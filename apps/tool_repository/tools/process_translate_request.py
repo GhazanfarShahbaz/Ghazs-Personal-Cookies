77cb16e12f36d31686fcdb4d40232f50ae988c43
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-08-05 17:34:36 +0000

    Updated: Pylint /home/ghaz/flask_gateway/apps/tool_repository/tools//process_translate_request.py

--- apps/tool_repository/tools/process_translate_request.py
+++ apps/tool_repository/tools/process_translate_request.py
@@ -10,12 +10,13 @@ Edit Log:
 
 from apps.tool_repository.tools.translate_utils import translate_line
 
+
 def process_translate(translate_form: dict) -> dict:
     """
     Processes a translation request.
 
     This function takes a dictionary `translate_form` containing the text to be translated,
-    the source language, and the target language. 
+    the source language, and the target language.
     It then calls the `translate_line` function with these parameters to perform the translation
     and returns a dictionary containing the translated text.
 
